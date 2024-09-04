de66e3b3493b43b2025eef2541c0313876df5ece
THERESIAOSAMOSIR <samosirtheresia23@gmail.com>
2024-09-04 21:27:09 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24055 - Theresia Samosir
+// 12S24055 - Theresia Samsosir 
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
